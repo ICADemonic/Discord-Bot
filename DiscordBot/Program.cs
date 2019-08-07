765d3bfd7eee39127f48e9f8361ea4045ed40f35
ICA-Demonic <harvey.hallwood@hotmail.com>
2019-08-07 12:41:58 +0100

    Did update of Program.cs
    Changed Hello World to Hello, Discord

--- DiscordBot/Program.cs
+++ DiscordBot/Program.cs
@@ -2,11 +2,11 @@
 
 namespace DiscordBot
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Discord");
         }
     }
 }
